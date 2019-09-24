485c90aa295eea2dad30951ee3c815dffc558396
Vlad Zhurakovskyi <vz@MacBook-Pro-Vlad.local>
2019-09-24 15:40:49 +0300

    add possibility edit tasks

--- src/components/main/todoList/todoItem/TodoItem.jsx
+++ src/components/main/todoList/todoItem/TodoItem.jsx
@@ -5,11 +5,18 @@ export default class TodoItem extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleCheckbox = this.toggleCheckbox.bind(this);
-        this.deleteItem = this.deleteItem.bind(this);
+        this.state = {
+            readOnly: true
+        };
+
+        this.toggleStatusTask = this.toggleStatusTask.bind(this);
+        this.deleteTask = this.deleteTask.bind(this);
+        this.editTask = this.editTask.bind(this);
+        this.editMode = this.editMode.bind(this);
+        this.saveChange = this.saveChange.bind(this);
     }
 
-    toggleCheckbox() {
+    toggleStatusTask() {
         const { todoId, todoStatus } = this.props;
 
         firebase.firestore().collection("todo_list").doc(todoId).update({
@@ -17,20 +24,39 @@ export default class TodoItem extends Component {
         });
     }
 
-    deleteItem() {
+    deleteTask() {
         const { todoId } = this.props;
 
         firebase.firestore().collection("todo_list").doc(todoId).delete();
     }
 
+    editTask(event) {
+        const { todoId } = this.props;
+
+        firebase.firestore().collection("todo_list").doc(todoId).update({
+            task: event.target.value
+        });
+    }
+
+    editMode() {
+        this.setState({
+            readOnly: !this.state.readOnly
+        })
+    }
+
+    saveChange(event) {
+        event.keyCode === 13 && this.setState({readOnly: true})
+    }
+
     render() {
         const { todoTask, todoStatus, index } = this.props;
+        const { readOnly } = this.state;
 
         return(
             <div className="todo-item">
                 <div className="todo-item__info">
                     <input
-                        onChange={this.toggleCheckbox}
+                        onChange={this.toggleStatusTask}
                         className="todo-item__checkbox_hidden"
                         type="checkbox"
                         id={`statusTask ${ index }`}
@@ -39,18 +65,30 @@ export default class TodoItem extends Component {
                         checked={ todoStatus }
                     />
 
-                    <label className="todo-item__checkbox" htmlFor={`statusTask ${ index }`}>
+                    <label
+                        className="todo-item__checkbox"
+                        htmlFor={`statusTask ${ index }`}>
                         {todoStatus && <i className="icon icon-tick"/>}
                     </label>
 
-                    <label
-                        className={`todo-item__label ${todoStatus && 'todo__checked'}`}
-                        htmlFor={`statusTask ${ index }`}>
-                        { todoTask }
-                    </label>
+                    <input
+                        onChange={this.editTask}
+                        onKeyDown={this.saveChange}
+                        className={`todo-item__label ${!readOnly ? 'todo__readonly' : ''} ${todoStatus ? 'todo__checked' : ''}`}
+                        value={todoTask} readOnly={readOnly}/>
+
+                    {/*<label*/}
+                    {/*    className={`todo-item__label ${todoStatus && 'todo__checked'}`}*/}
+                    {/*    htmlFor={`statusTask ${ index }`}>*/}
+                    {/*    { todoTask }*/}
+                    {/*</label>*/}
                 </div>
-                <button className="btn todo-item__edit">edit</button>
-                <button className="btn todo-item__delete" onClick={this.deleteItem}>
+
+                <button className="btn todo-item__edit" onClick={this.editMode}>
+                    <i className="icon icon-edit"/>
+                </button>
+
+                <button className="btn todo-item__delete" onClick={this.deleteTask}>
                     <i className="icon icon-backspace-outline"/>
                 </button>
             </div>
